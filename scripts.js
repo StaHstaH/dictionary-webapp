716e7828fa790dbf0797770baec1bd4b2a4772fb
StaHstaH <stanislaw.sipowicz@backdoorstudios.pl>
2024-02-13 18:58:51 +0100

    Displaying definitions, synonyms, antonyms

--- scripts.js
+++ scripts.js
@@ -45,11 +45,11 @@ function displayWord(json) {
 }
 
 function displayMeaning(meaning) {
-  //TODO
-  //find the mother element
-  //clone the mother element and fill it with content
   let meaningTemplate = document.getElementById('meaning-template');
   let domElement = meaningTemplate.cloneNode(true);
+  let definitionTemplate = domElement.querySelector('ul li');
+  let definitionList = domElement.querySelector('ul');
+  
   domElement.removeAttribute('id');
   domElement.classList.remove('hide');
   
@@ -59,5 +59,43 @@ function displayMeaning(meaning) {
   let container = document.getElementById("meanings");
   container.appendChild(domElement);
 
+  for (const definition of meaning.definitions) {
+     displayDefinition(definition, definitionTemplate, definitionList);
+   }
+  definitionTemplate.remove();
   console.log(meaning);
+
+  handleSynonymsAntonyms(meaning.synonyms, domElement, '.synonyms');
+  handleSynonymsAntonyms(meaning.antonyms, domElement, '.antonyms');
+}
+
+function handleSynonymsAntonyms(wordList, domElement, className) {
+  if (wordList.length > 0) {
+    let synonymList = domElement.querySelector(className + " p");
+
+    for (const synonym of wordList) {
+      let synonymLink = document.createElement("a");
+      synonymLink.innerHTML = synonym;
+      synonymList.appendChild(synonymLink);
+    }
+  } else {
+    let synonymSection = domElement.querySelector(className);
+    synonymSection.remove();
+  }
+}
+
+
+function displayDefinition(definition, template, parentNode) {
+  let domElement = template.cloneNode(true);
+  let definitionText = domElement.querySelector('p.definition-text');
+  definitionText.innerHTML = definition.definition;
+
+let exampleText = domElement.querySelector("p.example");
+  if(definition.example) {
+    exampleText.innerHTML = definition.example;
+  } else {
+    exampleText.remove();
+  }
+
+  parentNode.appendChild(domElement);
 }
