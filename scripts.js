ff762684c3c0505aa62197ceb827363cb14cab24
StaHstaH <stanislaw.sipowicz@backdoorstudios.pl>
2024-02-27 19:57:45 +0100

    Fixed the issue of multiple reponses

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-const input = document.getElementById('wordsearch');
-const playback = document.querySelector('#word-definition img');
+const input = document.getElementById("wordsearch");
+const playback = document.querySelector("#word-definition img");
 
 input.addEventListener("keyup", () => {
   console.log(input.value + "dupa");
-  searchWord(input.value);
+  let query = input.value.trim();
+  if(query.length > 0) {
+    searchWord(query);
+    input.classList.remove("input-error");
+  } else {
+    input.classList.add("input-error");
+  }
+
 });
 
 playback.addEventListener("click", () => {
@@ -11,63 +18,84 @@ playback.addEventListener("click", () => {
   phoneticPlayback.play();
 });
 
-
+let currentAbortController = null;
 const searchWord = async (word) => {
-  const response = await fetch(
-    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-    
-  );
-  const json = await response.json(); 
-  console.log(json);
-  displayWord(json);
+  if (currentAbortController) {
+    currentAbortController.abort();
+  }
+  currentAbortController = new AbortController();
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, {
+    signal: currentAbortController.signal,
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      displayWord(json);
+    })
+    .catch((error) => {
+      // If the request was aborted, do nothing
+      if (error.name === "AbortError") return;
+      // Otherwise, handle the error here or throw it back to the console
+      throw error;
+    });
 };
 
 function displayWord(json) {
   let errorSection = document.querySelector("#error");
-  let playButton = document.querySelector("#play-button");
+  let wordSection = document.querySelector("#word-definition");
 
-  if(json.title) {
+  if (json.title) {
     let title = document.querySelector("#error h3");
-    
+
     title.innerHTML = json.title;
     errorSection.classList.remove("hide");
     errorSection.classList.add("error-flex");
-    playButton.classList.add("hide");
+    wordSection.classList.add("hide");
     return;
-  } errorSection.classList.add("hide");
-    playButton.classList.remove("hide");
-    
+  }
+  errorSection.classList.add("hide");
+  wordSection.classList.remove("hide");
 
   let phonetic = findPhonetics(json[0]);
-  let phoneticDisplay = document.querySelector('#word-result h3');
-  let phoneticPlayback = document.getElementById('pronunciation');
+  let phoneticDisplay = document.querySelector("#word-result h3");
+  let phoneticPlayback = document.getElementById("pronunciation");
+  let playButton = document.getElementById("play-button");
 
-  phoneticPlayback.src = phonetic.audio;
-
-  phoneticDisplay.innerHTML = phonetic.text;
-
-  let meanings = document.getElementById('meanings');
-  meanings.replaceChildren();
   
- 
+  if(phonetic.text) {
+    phoneticDisplay.innerHTML = phonetic.text;
+    phoneticDisplay.classList.remove("hide");
+  } else {
+    phoneticDisplay.classList.add("hide");
+  }
+
+  if(phonetic.audio) {
+    playButton.classList.remove("hide");
+    phoneticPlayback.src = phonetic.audio;
+  } else {
+    playButton.classList.add("hide");
+  }
+
+  let meanings = document.getElementById("meanings");
+  meanings.replaceChildren();
+
   for (const meaning of json[0].meanings) {
     displayMeaning(meaning);
   }
-
 
   let result = document.getElementById("word-search-result");
   result.innerHTML = json[0].word;
 }
 
 function displayMeaning(meaning) {
-  let meaningTemplate = document.getElementById('meaning-template');
+  let meaningTemplate = document.getElementById("meaning-template");
   let domElement = meaningTemplate.cloneNode(true);
-  let definitionTemplate = domElement.querySelector('ul li');
-  let definitionList = domElement.querySelector('ul');
-  
-  domElement.removeAttribute('id');
-  domElement.classList.remove('hide');
-  
+  let definitionTemplate = domElement.querySelector("ul li");
+  let definitionList = domElement.querySelector("ul");
+
+  domElement.removeAttribute("id");
+  domElement.classList.remove("hide");
+
   let partOfSpeech = domElement.querySelector("h2");
   partOfSpeech.innerHTML = meaning.partOfSpeech;
 
@@ -75,13 +103,13 @@ function displayMeaning(meaning) {
   container.appendChild(domElement);
 
   for (const definition of meaning.definitions) {
-     displayDefinition(definition, definitionTemplate, definitionList);
-   }
+    displayDefinition(definition, definitionTemplate, definitionList);
+  }
   definitionTemplate.remove();
   console.log(meaning);
 
-  handleSynonymsAntonyms(meaning.synonyms, domElement, '.synonyms');
-  handleSynonymsAntonyms(meaning.antonyms, domElement, '.antonyms');
+  handleSynonymsAntonyms(meaning.synonyms, domElement, ".synonyms");
+  handleSynonymsAntonyms(meaning.antonyms, domElement, ".antonyms");
 }
 
 function handleSynonymsAntonyms(wordList, domElement, className) {
@@ -90,17 +118,17 @@ function handleSynonymsAntonyms(wordList, domElement, className) {
 
     for (const synonym of wordList) {
       let synonymLink = document.createElement("a");
-      let space = document.createTextNode('\xa0\xa0\xa0 ');
+      let space = document.createTextNode("\xa0\xa0\xa0 ");
       synonymLink.innerHTML = synonym;
-      synonymLink.addEventListener('click' , () => {
+      synonymLink.addEventListener("click", () => {
         searchWord(synonym);
         window.scrollTo({
           top: 0,
           left: 0,
-          behavior: 'smooth',
+          behavior: "smooth",
         });
         input.value = synonym;
-      })
+      });
       synonymList.appendChild(synonymLink);
       synonymList.appendChild(space);
     }
@@ -112,11 +140,11 @@ function handleSynonymsAntonyms(wordList, domElement, className) {
 
 function displayDefinition(definition, template, parentNode) {
   let domElement = template.cloneNode(true);
-  let definitionText = domElement.querySelector('p.definition-text');
+  let definitionText = domElement.querySelector("p.definition-text");
   definitionText.innerHTML = definition.definition;
 
-let exampleText = domElement.querySelector("p.example");
-  if(definition.example) {
+  let exampleText = domElement.querySelector("p.example");
+  if (definition.example) {
     exampleText.innerHTML = definition.example;
   } else {
     exampleText.remove();
@@ -125,14 +153,14 @@ let exampleText = domElement.querySelector("p.example");
   parentNode.appendChild(domElement);
 }
 
-function findPhonetics (json) {
+function findPhonetics(json) {
   for (const phonemes of json.phonetics) {
-    if(phonemes.audio) {
+    if (phonemes.audio) {
       return {
         text: phonemes.text,
-        audio: phonemes.audio
+        audio: phonemes.audio,
       };
-    } 
+    }
   }
   return json.phonetic;
 }
